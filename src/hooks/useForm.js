99777a4c3a870866d3761bf9d4b10b74dfe04030
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-08-04 22:50:43 -0300

    Inicio aula 5

---
+++ src/hooks/useForm.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+export function useForm(valoresIniciais){
+    const [values,setValues] =  useState(valoresIniciais);
+    
+    function setValue(chave, valor){
+      // chave: nome, descrição, em fim objeto que for alterar
+       setValues({
+          ...values,
+          [chave]: valor, // nome:'valor' do input         
+       })
+    }
+  
+  function handleChange(infosDoEvento){
+     setValue(infosDoEvento.target.getAttribute('name'), 
+     infosDoEvento.target.value
+     );
+     } 
+  
+     function clearForm(){
+        setValue(valoresIniciais);
+     }
+  
+     return {
+       values,
+      handleChange,
+      clearForm,
+     };
+  }
+
+export default useForm;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,36 +2,24 @@ import React,{ useState, useEffect } from 'react';
 import PageBase from '../../../components/PageBase';
 import {Link} from 'react-router-dom';
 import FormField from '../../../components/FormField';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria(){
              
   const  valoresIniciais ={
-          nome:'',
+          titulo:'',
           descricao :'',
           cor:'',
        }
 
-  const [values,setValues] =  useState(valoresIniciais);
+  const { handleChange,values, clearForm} = useForm(valoresIniciais);     
+
   const [categorias, setCategorias] = useState([]);
-
-    function setValue(chave, valor){
-         // chave: nome, descrição, em fim objeto que for alterar
-          setValues({
-             ...values,
-             [chave]: valor, // nome:'valor' do input         
-          })
-       }
-
-   function handleChange(infosDoEvento){
-        setValue(infosDoEvento.target.getAttribute('name'), 
-        infosDoEvento.target.value
-        );
-      } 
 
    useEffect(() => {
       console.log('alo alo');
       const URL = window.location.hostname.includes('localhost')
-      ? 'http://localhost:8080/categorias' 
+      ? 'http://localhost:8000/categorias' 
       : 'https://prflix.herokuapp.com/categorias';
       fetch(URL)
       .then(async(respotaDoServidor) => {
@@ -39,28 +27,11 @@ function CadastroCategoria(){
         setCategorias([...resposta,]);
       });
 
-      /*setTimeout(() => {
-        setCategorias([
-            ...categorias,
-            {
-              id: 1,
-              nome: 'XXXXXX',
-              descricao: 'YYYYYYY',
-              cor: '#cbd1ff',
-            },
-            {
-              id: 2,
-              nome: 'bbbbbb',
-              descricao: 'fffffff',
-              cor: '#cbd1ff',
-            },
-          ]);
-        }, 4 * 1000);*/
-        } ,[]);
+      } ,[]);
 
     return (
       <PageBase>
-         <h1>Cadastro de Categoria : {values.nome}</h1>
+         <h1>Cadastro de Categoria : {values.titulo}</h1>
     
     <form onSubmit={function handleSubmit(infosDoEvento){
         infosDoEvento.preventDefault();
@@ -68,14 +39,14 @@ function CadastroCategoria(){
           ...categorias,
           values
         ]);
-        setValue(valoresIniciais)
+        clearForm();
        }}>
 
     <FormField
           label ="Nome da Categoria :"
           type="text"
-          name ="nome"
-          value ={values.nome}
+          name ="titulo"
+          value ={values.titulo}
           onChange ={handleChange} 
     />
 
@@ -109,8 +80,8 @@ function CadastroCategoria(){
         <ul>
           {categorias.map((categoria, indice) => {
               return(
-                <li key={`${categoria.nome}${indice}`}>
-                  {categoria.nome}
+                <li key={`${categoria.titulo}${indice}`}>
+                  {categoria.titulo}
                 </li>
               )})}
         </ul>
