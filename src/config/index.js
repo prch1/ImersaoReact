8621f3db5e92c98dff01c360e4512ab82a76339f
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-08-12 00:43:40 -0300

    heroko III

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8000'
-  : 'https://prflix.herokuapp.com/categorias/';
+  : 'https://prflix.herokuapp.com/';
 
 export default {
   URL_BACKEND_TOP,
